7f20535292ffbe4c1e7762301571247f5e861757
Vanillwa <yjh30448813@gmail.com>
2024-03-04 23:40:13 +0900

    240304

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -5,13 +5,15 @@ import { Route, Routes, Link, useNavigate, Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Main from './pages/Main';
 import Login from './pages/Login';
+import { useState } from 'react';
 function App() {
+    const [user, setUser] = useState(null);
     return (
         <>
             <Header></Header>
             <Routes>
                 <Route path='/' element={<Main></Main>}></Route>
-                <Route path='/login' element={<Login></Login>}></Route>
+                <Route path='/login' element={<Login setUser={setUser}></Login>}></Route>
             </Routes>
         </>
     );
